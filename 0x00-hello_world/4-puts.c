75f84b36cc82f270f927d18ed5a9bd64f9e7fb17
Agustin <bolioliagustin@gmail.com>
2021-06-17 18:29:30 -0700

    s

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+   /**
+    * main - print message with puts
+    *
+    * Return: Always 0 (Success)
+    */
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+
+  return (0);
+}
