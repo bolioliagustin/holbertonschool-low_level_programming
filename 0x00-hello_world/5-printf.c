77127e98549eb06e432cd945ac8f119c00adae45
Agustin <bolioliagustin@gmail.com>
2021-06-20 08:15:59 -0700

    s

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+*main - print a message with printf
+*
+*Return: end program
+*/
 int main(void)
 {
-printf("with proper grammar, but the outcome is a pice of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
