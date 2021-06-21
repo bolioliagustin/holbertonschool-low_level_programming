5a25ad96ddf1e1a3fafcf49539945d554af75634
Agustin <bolioliagustin@gmail.com>
2021-06-21 16:09:29 -0700

    s

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+/**
+  * main - Entry point
+  *
+  * prints a string
+  *
+  * Return: 0 if the function went through to competion successfully
+  */
+int main(void)
+{
+write(2, "and that pice of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return(1);
+}
