0146e14f62d01a0d6bfaabb0734488e1fc7fbba8
Agustin <bolioliagustin@gmail.com>
2021-06-22 10:41:29 -0700

    s

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+* print_alphabet - check the code for Holberton School students.
+*
+* Return: Always 0.
+**/
+void print_alphabet(void)
+{
+char az;
+for (az = 'a'; az <= 'z'; az++)
+{
+_putchar (az);
+_putchar ('\n');
+}
+}
+
