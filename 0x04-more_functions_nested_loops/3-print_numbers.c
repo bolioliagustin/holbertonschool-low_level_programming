453a5d58074718b16a59d670c836ca03f53fe73c
Agustin <bolioliagustin@gmail.com>
2021-06-24 11:54:56 -0700

    s

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+*print_numbers - Entry point
+*/
+void print_numbers(void)
+{
+int c;
+for (c = 0; c < 10 ; c++)
+{
+	_putchar(c + '0');
+}
+	_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,6 @@
+#include "holberton.h"
+/**
+* print_most_numbers -  the numbers, 0 to 9 without 2 and 4
+* Return: void
+*/
+
