ef0cbbb989a9c74335f0b9d3b6a42fc70bdaa40e
bolioliagustin <85562073+bolioliagustin@users.noreply.github.com>
2021-06-29 21:44:01 -0300

    sa

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - generates random valid passwords for the program 101-crackme
+ * Description: generates random valid passwords for the program 101-crackme
+ * Return "0"
+ */
+
+int main(void)
+{
+return (0);
+}
