092c05fc6370740f011b69b6c8a98d6a6213bd2d
root <bolioliagustin@gmail.com>
2021-07-01 09:39:32 -0700

    a

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,5 +1,23 @@
 #include "holberton.h"
+/**
+* _strncat - function is similar to the _strcat
+* @dest: char pointer
+* @src: char pointer
+* @n: int
+* Return: dest
+*/
 
 char *_strncat(char *dest, char *src, int n)
 {
 
+int i, j;
+
+for (i = 0; *(dest + i); i++)
+	;
+
+dest[i] = ' ';
+for (j = 0; j < n && src[j] != '\0'; j++)
+	dest[i + j] = src[j];
+dest[i + j] = '\0';
+return (dest);
+}
