beef0346f87e43ce97ddf46a32c324e636b41bbb
bolioliagustin <bolioliagustin@gmail.com>
2021-07-01 17:15:40 -0700

    s

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+* _strncpy - copy a string
+* Description: emulate strncpy
+* @dest: final string
+* @src: initial string
+* @n: amount of bytes
+* Return: dest
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int i = 0;
+
+while (*(src + i) != '\0' && i < n)
+{
+	*(dest + i) = *(src + i);
+	i++;
+}
+*(dest + i) = *(src + i);
+return (dest);
+}
