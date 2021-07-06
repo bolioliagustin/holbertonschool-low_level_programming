a36908f530a5ac7c47439237d00534419f69582d
bolioliagustin <bolioliagustin@gmail.com>
2021-07-05 17:01:58 -0700

    s

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _strpbrk - Search a string for any of a set of bytes.
+ * @s: string
+ * @accept: string to match
+ * Return: Pointer to the byte in `s` that matches one of the bytes in `accept`
+ * or NULL if no such byte is found.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+
+int i = 0;
+int j;
+char *p;
+
+while (s[i] != '\0')
+{
+	j = 0;
+
+	while (accept[j] != '\0')
+	{
+		if (s[i] == accept[j])
+		{
+			p = &s[i];
+			return (p);
+
+		}
+		j++;
+	}
+	i++;
+}
+return (NULL);
+}
