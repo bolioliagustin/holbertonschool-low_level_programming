5ecb55b1e988edf9907c692918b2fb36339f2af4
bolioliagustin <bolioliagustin@gmail.com>
2021-07-07 10:05:33 -0700

    a

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,10 +1,13 @@
 #include "holberton.h"
-
+/**
+ *_print_rev_recursion - print string
+ *@s: is a string
+ */
 void _print_rev_recursion(char *s)
 {
-    if (*s)
-    {
-      _print_rev_recursion(s + 1);
-      _putchar(*s);   
-    } 
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,12 +1,16 @@
 #include "holberton.h"
-
+/**
+ *_strlen_recursion - return a length string
+ *@s: string
+ *Return: length  string
+ */
 int _strlen_recursion(char *s)
 {
-  int i;  
-    if (*s)
-    {
-      i = _strlen_recursion(++s) + 1;
-        return (i);
-    }    
+int i;
+	if (*s)
+	{
+		i = _strlen_recursion(++s) + 1);
+			return (i);
+	}
 return (0);
 }
