e1d68ea75ac25d3a328ea5c4e92a2096dd6b3bcc
bolioliagustin <bolioliagustin@gmail.com>
2021-07-09 14:46:43 -0700

    #

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * main - Entry point
+ *@argc: number of arguments
+ *@argv: string with arguments
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char *argv[])
 {
-    int i;
-    {
-        if (argc == 3)
-        {
-         i = (atoi(argv[1]) * atoi(argv[2]));
-         printf("%d \n", i);
-         return (0);
-        }
-        else
-        {    
-            printf("Error\n");           
-            return (1);
-        }
-    }
-}
+	int i;
+
+	(void)argv;
+	if (argc == 3)
+	{
+		i = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", i);
+	}
+	else
+	{
+		puts("Error");
+	}
+	return (0);
+
