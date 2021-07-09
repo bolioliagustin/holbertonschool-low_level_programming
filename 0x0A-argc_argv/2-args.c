c6577678b5616466199519826cc18075d74123dc
bolioliagustin <bolioliagustin@gmail.com>
2021-07-09 08:18:46 -0700

    a

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: wopa
+ * @argv: sdkna
+ *
+ * Description: Show a message blablabla
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+int i;
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+return (0);
+}
