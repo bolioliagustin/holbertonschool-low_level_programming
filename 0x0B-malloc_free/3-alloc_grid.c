9ce8530b31bffa4a293077547fa73b851a3c0cd8
bolioliagustin <85562073+bolioliagustin@users.noreply.github.com>
2021-07-14 23:32:23 -0300

    a

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+
+/**
+ * alloc_grid - Create a 2 dimensionals array
+ * @width: Width of grid, aka # of columns
+ * @height: Height of grid, aka # of rows
+ *
+ * Return: Pointer to 2D array, NULL if it fails
+ */
+int **alloc_grid(int width, int height)
+{
+	int **g;
+	int j, i;
+
+	j = 0;
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	grid = malloc(height * sizeof(*g));
+	if (g == NULL)
+	{
+		free(g);
+		return (NULL);
+	}
+	while (j < height)
+	{
+		grid[j] = malloc(width * sizeof(**g));
+		if (g[j] == NULL)
+		{
+			j--;
+			while (j >= 0)
+			{
+				free(g[j]);
+				j--;
+			}
+			free(g);
+			return (NULL);
+		}
+		i = 0;
+		while (i < width)
+		{
+			grid[j][i] = 0;
+			i++;
+		}
+		j++;
+	}
+	j = 0;
+	return (g);
+}
