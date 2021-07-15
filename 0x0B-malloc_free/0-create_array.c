a59b9de535ab117e9981383507978e0165e7e9ca
bolioliagustin <bolioliagustin@gmail.com>
2021-07-14 19:17:05 -0700

    s

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,22 +1,28 @@
 #include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * create_array - create an array of chars
+ * @size: size of array
+ * @c: char
+ * Return: char pointer to malloc o NULL if error
+ */
 
 char *create_array(unsigned int size, char c)
 {
-    char *p = NULL;
-    unsigned int i = 0;
-    if (size > 0)
-    {
-        p = malloc(sizeof(char)*size);
-        if(!p)
-        {
-            return (p);
-        }
-        for (i = 0;i <= size; i++)
-        {
-            p[i] = c;
-        }
-    }
-    return(p);
+char *p = NULL;
+unsigned int i = 0;
+if (size > 0)
+{
+	p = malloc(sizeof(char) * size);
+	if (!p)
+	{
+		return (p);
+	}
+	for (i = 0; i <= size; i++)
+	{
+		p[i] = c;
+	}
+}
+return (p);
 }
--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,24 +2,30 @@
 #include <stdio.h>
 #include "holberton.h"
 
+/**
+  * _strdup - Duplicate a string
+  * @str: the string to duplicate
+  *
+  * Return: the string duplicated
+  */
+
 char *_strdup(char *str)
 {
-    char *p = NULL;
-    int i = 0;
-    int l = 0;
+char *p = NULL;
+int i = 0;
+int l = 0;
 
-    for (l = 0; *(str+l) != '\0'; l++) 
-    {
-
-    }
-    p = malloc(sizeof(char)*l);
-    if (p)
-    {
-        for (i = 0; i < l; i++)
-        {
-            p[i] = str[i];     
-        }
-        p[l] = '\0';
-    }
-    return (p);
+for (l = 0; *(str + l) != '\0'; l++)
+{
+}
+p = malloc(sizeof(char) * l);
+if (p)
+{
+	for (i = 0; i < l; i++)
+	{
+		p[i] = str[i];
+	}
+	p[l] = '\0';
+}
+return (p);
 }
