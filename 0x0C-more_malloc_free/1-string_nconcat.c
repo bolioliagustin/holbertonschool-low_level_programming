be4939a5ee18ff6161c6665034aa1acb49e92365
bolioliagustin <bolioliagustin@gmail.com>
2021-07-16 07:11:31 -0700

    malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,28 +13,33 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 unsigned int i, j, k;
 char *p = NULL;
 
-for (i = 0; s1[i] != '\0'; i++)
-	;
-for (j = 0; s2[j] != '\0'; j++)
-	;
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s1 = "";
 
+for (i = 0; s1[i] != '\0'; i++)
+{}
+for (j = 0; s2[j] != '\0'; j++)
+{}
 if (j > n)
-j = n
-;
+{
+j = n;
+}
 
 p = malloc(sizeof(char) * (i + j + 1));
-	if (!p)
-	{
-		return (NULL);
-	}
-	for (k = 0; k < i; k++)
-	{
-		p[k] = s1[k];
-	}
-	for (k = 0; k < j; k++)
-	{
-		p[k + i] = s2[k];
-	}
-	p[i + j] = '\0';
+if (!p)
+{
+return (NULL);
+}
+for (k = 0; k < i; k++)
+{
+p[k] = s1[k];
+}
+for (k = 0; k < j; k++)
+{
+p[k + i] = s2[k];
+}
+p[i + j] = '\0';
 return (p);
 }
