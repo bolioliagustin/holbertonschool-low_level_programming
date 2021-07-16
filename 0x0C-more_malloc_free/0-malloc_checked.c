f92397145d5ad87f727457ddeda0097b71c8583f
bolioliagustin <bolioliagustin@gmail.com>
2021-07-16 06:33:41 -0700

    s

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -8,11 +8,11 @@
  */
 void *malloc_checked(unsigned int b)
 {
-  void *p;
-  p = NULL;
-  p = malloc(b);
+void *p;
+p = NULL;
+p = malloc(b);
 
-  if (p == NULL)
+if (p == NULL)
 	exit(98);
 
 return (p);
