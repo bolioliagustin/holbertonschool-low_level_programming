39ae9ad284fdda087f3293e2336f782ef779b82d
bolioliagustin <bolioliagustin@gmail.com>
2021-07-19 09:21:25 -0700

    s

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * init_dog - Entry point
+ * @d: sasd
+ * @name: name dog
+ * @age: age dog
+ * @owner: owner
+ *
+ * Description: message blabalskaka
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+struct dog;
+
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,8 @@
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
