59dcea7869d102d253a1a8b64ef953a5c41bd533
bolioliagustin <bolioliagustin@gmail.com>
2021-07-20 05:38:35 -0700

    s

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,16 @@
 #ifndef STRUCFILE
 #define STRUCFILE
+/**
+* struct dog - Dog´s structure
+* @name: The name of the dog
+* @age: The age of the dog
+* @owner: The owner of the dog
+*/
 struct dog
 {
 char *name;
 float age;
 char *owner;
 };
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
