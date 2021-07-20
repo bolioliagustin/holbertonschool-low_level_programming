b9912abe8ad60d09eb8a54d97a95bea653759510
bolioliagustin <bolioliagustin@gmail.com>
2021-07-20 14:08:10 -0700

    s

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,23 +11,35 @@ char *str_concat(char *s1, char *s2)
 {
 char *p = NULL;
 int i, j;
-for (i = 0; s1[i]; i++)
+if (s1 == NULL)
+{
+	s1 = "";
+}
+if (s2 == NULL)
+{
+	s2 = "";
+}
+for (i = 0; s1[i] != '\0'; i++)
 	;
-for (i = 0; s2[j]; j++)
+for (i = 0; s2[j] != '\0'; j++)
 	;
-p = malloc(i + j);
-if (p)
+p = malloc((i + j) + 1);
+if (p == NULL)
+{
+	return(NULL);
+}
+else
 {
 	if (i)
 	{
-		for (i = 0; s1[i]; i++)
+		for (i = 0; s1[i] != '\0'; i++)
 		{
 			p[i] = s1[i];
 		}
 	}
 	if (j)
 	{
-		for (j = 0; s2[j]; j++)
+		for (j = 0; s2[j] != '\0'; j++)
 		{
 			p[i + j] = s2[j];
 		}
@@ -36,4 +48,3 @@ if (p)
 }
 return (p);
 }
-
