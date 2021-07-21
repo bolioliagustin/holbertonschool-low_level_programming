c3a29fc6e7fad9c8ad312e8e25e36439c254f130
bolioliagustin <bolioliagustin@gmail.com>
2021-07-20 17:43:19 -0700

    as

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* argstostr - Creates a string from the concatenation of all blabla
+* @ac: Number of arguments
+* @av: The array of arguments
+* Return: a pointer to a new string.
+*/
+char *argstostr(int ac, char **av)
+{
+	int i, j;
+	char *p;
+	int l = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; *(*(av + i) + j) != '\0'; j++)
+			l++;
+	}
+	str = malloc(sizeof(char) * (l + i + 1));
+	if (p)
+	{
+		length = 0;
+		for (i = 0; i < ac; i++)
+		{
+			for (j = 0; *(*(av + i) + j) != '\0'; j++)
+			{
+				*(p + l) = *(*(av + i) + j);
+				l++;
+			}
+			*(p + length) = '\n';
+			l++;
+		}
+		*(p + length) = '\0';
+		return (p);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
