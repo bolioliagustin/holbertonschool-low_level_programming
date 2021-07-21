96e35e9a874b4c0a959cc6c58b93656aef01feca
bolioliagustin <bolioliagustin@gmail.com>
2021-07-20 17:25:48 -0700

    s

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,50 +1,49 @@
-#include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "holberton.h"
 /**
- * str_concat - concatenate strings using malloc
- * @s1: string 1
- * @s2: string 2
- * Return: pointer of concat string
- */
+* *str_concat - concatena un array
+* @s1: array
+* @s2: array
+*
+*Return: Always 0.
+*/
 char *str_concat(char *s1, char *s2)
 {
-char *p = NULL;
-int i, j;
-if (s1 == NULL)
-{
-	s1 = "";
-}
-if (s2 == NULL)
-{
-	s2 = "";
-}
-for (i = 0; s1[i] != '\0'; i++)
-	;
-for (i = 0; s2[j] != '\0'; j++)
-	;
-p = malloc((i + j) + 1);
-if (p == NULL)
-{
-	return(NULL);
-}
-else
-{
-	if (i)
+	char *p;
+	int i, j, k, l;
+
+	if (s1 == NULL)
 	{
-		for (i = 0; s1[i] != '\0'; i++)
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	for (i = 0; s1[i] != '\0'; i++)
+	{}
+
+	for (j = 0; s2[j] != '\0'; j++)
+	{}
+
+	p = malloc((sizeof(*s1) * i) + (sizeof(*s2) * j + 1));
+
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+
+	for (k = 0,l = 0; k < (i + j + 1); k++)
+	{
+		if (k < i)
 		{
-			p[i] = s1[i];
+			p[k] = s1[k];
+		}
+		else
+		{
+			p[k] = s2[l++];
 		}
 	}
-	if (j)
-	{
-		for (j = 0; s2[j] != '\0'; j++)
-		{
-			p[i + j] = s2[j];
-		}
-	}
-	p[i + j] = 0;
-}
-return (p);
+	return (p);
 }
