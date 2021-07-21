e3ccee861b7ab73f992ab97b76b3c03de6f06092
bolioliagustin <bolioliagustin@gmail.com>
2021-07-21 16:21:32 -0700

    s

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+/**
+* op_add - addition operator
+* @a: first int
+* @b: second int
+* Return: sum of 2 numbers
+*/
+int op_add(int a, int b)
+{
+	return(a + b);
+}
+
+/**
+ * op_add - addition operator
+ * @a: first int
+ * @b: second int
+ * Return: subtract of 2 numbers
+ */
+int op_sub(int a, int b)
+{
+	return(a - b);
+}
+/**
+ * op_add - addition operator
+ * @a: first int
+ * @b: second int
+ * Return: multiply of 2 numbers
+ */
+int op_mul(int a, int b)
+{
+	return(a * b);
+}
+/**
+ * op_add - addition operator
+ * @a: first int
+ * @b: second int
+ * Return: divide of 2 numbers
+ */
+int op_div(int a, int b)
+{
+	return(a / b);
+}
+/**
+ * op_add - addition operator
+ * @a: first int
+ * @b: second int
+ * Return: modulo of 2 numbers
+ */
+int op_mod(int a, int b)
+{
+	return(a % b);
+}
