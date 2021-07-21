9a3c9c112f035a136d08e055ceb7ab290af3fc8d
bolioliagustin <bolioliagustin@gmail.com>
2021-07-21 09:21:18 -0700

    a

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,10 +1,19 @@
 #include "function_pointers.h"
-
+/**
+ *array_iterator - interators of array
+ *@array: array of integers
+ *@action: fuction pointer
+ *@size: size of array
+ */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	size_t i
-	for (i = 0; array[i] != '\0'; i++)
+	size_t i;
+
+	if (array && action && size > 0)
 	{
-		action(array[i]);	
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
 	}
 }
