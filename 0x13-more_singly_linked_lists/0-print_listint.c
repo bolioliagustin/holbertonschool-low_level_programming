9dcc7168159c37b29beaf37d13f5f2623368b36b
bolioliagustin <bolioliagustin@gmail.com>
2021-08-02 16:01:46 -0700

    s

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+* print_listint - prints all elements of a list
+* @h: pointer to list
+* Return: lu, number of nodes
+*/
+size_t print_listint(const listint_t *h)
+{
+size_t a = 0;
+if (h)
+{
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h ->next;
+	a++;
+	}
+}
+return(a);
+}
---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+* listint_len - Write the number of elements in a linked
+* @h pointer a list 
+* Return : Number of elements
+*/
+size_t listint_len(const listint_t *h)
+{
+size_t a;
+a = 0;
+if (h)
+{
+	while (h)
+	{
+		a++;
+		h = h ->next;
+	}	
+}
+return (a);
+}
